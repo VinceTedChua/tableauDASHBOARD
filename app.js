2aed48d90e33e8dc9d2f6bf9e32cd2e3e1c868ff
Vince Ted Matthew D. Chua <142372312+VinceTedChua@users.noreply.github.com>
2024-05-10 20:41:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let viz;
 //Add Share Link to Tableau Public in here
 const url = "https://public.tableau.com/views/Book1_17149890022240/AwesomeChocolateDashboardReport?:language=en-GB&publish=yes&:sid=&:display_count=n&:origin=viz_share_link";
 
-const ErikkaEnaje = document.getElementById('ErikkaEnaje');
+const VinceChua = document.getElementById('ErikkaEnaje');
 const options = {
     hideTabs: true,
     height: 1000,
@@ -17,7 +17,7 @@ const options = {
 };
 
 function openTableau() {
-    viz = new tableau.Viz(ErikkaEnaje, url, options);
+    viz = new tableau.Viz(VinceChua, url, options);
 }
 
 document.addEventListener("DOMContentLoaded", openTableau);
